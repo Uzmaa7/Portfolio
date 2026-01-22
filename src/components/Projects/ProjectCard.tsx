93c7873c7843028e22d44dd00c2c1bc9f4694704
Uzmaa7 <uk9971khan@gmail.com>
2026-01-22 18:27:35 +0530

    Implemented ProjectCard with support for both video and image media

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -9,6 +9,27 @@ interface ProjectCardProps {
 
 export default function ProjectCard({ mediaPath, isVideo, title, description }: ProjectCardProps) {
     return (
-        
+        <div className="">
+            {isVideo ? (
+                <video
+                    src={mediaPath}
+                    autoPlay
+                    muted
+                    loop
+                    playsInline
+                    className="rounded-lg w-full h-auto object-cover border-2 border-white"
+                />
+            ) : (
+                <img
+                    src={mediaPath}
+                    alt={title}
+                    className="rounded-lg w-full h-auto object-cover border-2 border-white"
+                />
+            )}
+
+            <p className="my-4 text-xl sm:text-2xl font-semibold text-gray-200">{title}</p>
+            <p className="font-medium text-gray-400">{description}</p>
+        </div>
+
     )
 }
