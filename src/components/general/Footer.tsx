6568fa8c1b38fdd2209b98c57189a8d5ce5afd4f
Uzmaa7 <uk9971khan@gmail.com>
2026-01-22 21:18:40 +0530

    feat: add footer component with dynamic year and styling - Created Footer with responsive padding and top border - Integrated dynamic year for copyright notice

---
+++ src/components/general/Footer.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Footer(){
+    return(
+        <footer className="border-t border-slate-800 py-10">
+            <div className="px-4 text-center text-gray-500 text-sm">
+                &copy; {new Date().getFullYear()} Uzma - Developer Portfolio
+
+            </div>
+        </footer>
+    )
+}
