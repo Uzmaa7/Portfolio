13db61b888bdf96d1e709097fb9d4d441e1d0cfd
Uzmaa7 <uk9971khan@gmail.com>
2026-01-22 13:24:14 +0530

    implement polymorphic Header for semantic flexibility
    - Create a reusable Header component with support for dynamic HTML tags (h1, h2).
    - Define HeaderProps interface to handle optional s prop and required 	itle.
    - Utilize a lookup object (sizeClasses) for scalable, tag-specific responsive sizing.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import HeroSection from "@/components/home/hero/HeroSection";
-
+import ServiceSection from "@/components/home/services/ServiceSection";
 export default function Home() {
   return (
    <>
    <HeroSection/>
+   <ServiceSection/>
    </>
   );
 }
